d0aeb2e41debddd85cbae35778409194fb26d179
Fausto Zecca <fzecca@alumicast.ca>
2021-03-11 11:35:29 -0500

    instructions added to credentials.h

--- include/credentials.h
+++ include/credentials.h
@@ -1,3 +1,7 @@
+//This file must be modified to work with your network
+//The path to this file has to be stated in main.h at the line:
+//      #include "......\credentials.h"
+
 const char* ssid = "SSID";                  //your wifi network's SSID
 const char* password =  "PASSWORD";         //your wifi access password
 const char* mqttServer = "###.###.###.###"; //your MQTT server's IP address
